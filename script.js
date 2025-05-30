445a4799ecd813d0ccdab01f61e245f6be8d8e7d
Carl Henrique <carlhenriquedev@gmail.com>
2025-05-30 16:18:57 -0300

    restore all

---
+++ script.js
@@ -0,0 +1,24 @@
+window.addEventListener("scroll", function(){
+
+    let header = document.querySelector("#header")
+    header.classList.toggle("rolagem", window.scrollY > 120)
+})
+
+// 
+
+
+
+function menuShow() {
+    const menuMobile = document.querySelector(".mobile-menu");
+    menuMobile.classList.toggle("open");
+
+    // Adiciona o listener apenas se abrir o menu
+    if (menuMobile.classList.contains("open")) {
+        const menuItems = menuMobile.querySelectorAll(".item-mobile");
+        menuItems.forEach(item => {
+            item.addEventListener("click", () => {
+                menuMobile.classList.remove("open");
+            });
+        });
+    }
+}
