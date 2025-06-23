25f75bfd43c3c1fc528f35ba707138604317b631
Carl Henrique <carlhenriquedev@gmail.com>
2025-06-22 21:40:34 -0300

    resolvi erro hash

--- bd/hash-gestor.php
+++ bd/hash-gestor.php
@@ -1,5 +1,8 @@
-/* execute este arquivo no local host para geral o hash */
+execute este arquivo no local host para geral o hash:
+
 <?php
+include("../config/connection.php");
+
 $senha = '123';
 $hash = password_hash($senha, PASSWORD_DEFAULT);
 echo $hash;
